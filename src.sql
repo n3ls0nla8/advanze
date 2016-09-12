a61f54e981f0e7f9399946bdd665e50e7ff05593
LAU KA FAI <nelsonjob8@gmail.com>
2016-09-12 11:56:18 +0800

    Update src.sql

--- src.sql
+++ src.sql
@@ -1,9 +1,6 @@
-set lin 500
+set lin 5000
 set pages 9999
+set trims on
 set ver off
-col OWNER format a8
-col NAME format a8
-col TYPE format a8
-col LINE format 999999
-col TEXT format a120 wrap
-SELECT * FROM ALL_SOURCE WHERE NAME LIKE UPPER('&source_name') AND TEXT LIKE '&code' ORDER BY LINE;
+set echo off
+SELECT TEXT FROM ALL_SOURCE WHERE NAME LIKE UPPER('&source_name') AND TEXT LIKE NVL('&code', '%') ORDER BY LINE;
