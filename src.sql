72b28dc8d11c21ca88461d4ec292c19a35cbb116
LAU KA FAI <nelsonjob8@gmail.com>
2016-09-13 15:13:58 +0800

    Update src.sql

--- src.sql
+++ src.sql
@@ -3,4 +3,4 @@ set pages 9999
 set trims on
 set ver off
 set echo off
-SELECT TEXT FROM ALL_SOURCE WHERE NAME LIKE UPPER('&source_name') AND TEXT LIKE NVL('&code', '%') ORDER BY LINE;
+SELECT TEXT FROM ALL_SOURCE WHERE NAME LIKE UPPER('&source_name') AND TYPE='PACKAGE BODY' ORDER BY LINE;
