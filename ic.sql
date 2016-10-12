067d036e90e9fb6857fc1ebb3566b0c4676781ef
LAU KA FAI <nelsonjob8@gmail.com>
2016-10-12 18:15:47 +0800

    Delete ic.sql

--- ic.sql
+++
@@ -1,5 +0,0 @@
-set lin 300
-col INDEX_OWNER format a12 wrap
-col TABLE_OWNER format a12 wrap
-col COLUMN_NAME format a30 wrap
-SELECT * FROM ALL_IND_COLUMNS WHERE TABLE_NAME LIKE upper('&table_name');
