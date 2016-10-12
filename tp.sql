12c4c6abf3c72059b1d813586d50094c242437ac
LAU KA FAI <nelsonjob8@gmail.com>
2016-10-12 18:16:49 +0800

    Delete tp.sql

--- tp.sql
+++
@@ -1,9 +0,0 @@
-set lin 800
-col GRANTOR format a20
-col GRANTEE format a20
-col TABLE_SCHEMA format a20
-col PRIVILEGE format a20
-col GRANTABLE format a10
-col HIERARCHY format a10
-
-SELECT * FROM ALL_TAB_PRIVS WHERE TABLE_NAME=upper('&table_name') ORDER BY GRANTEE;
